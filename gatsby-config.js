4a32d8a8778345b9e5c4438f639bc706bed93d99
braditamaa <62259218+braditamaa@users.noreply.github.com>
2020-04-09 23:31:03 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Braditama Tech',
+        short_name: 'Braditama',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
